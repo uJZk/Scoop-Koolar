8a5d3a43a5144d9136f8da64060bc9f7c3639c03
uJZk <34388941+uJZk@users.noreply.github.com>
2026-05-11 11:33:15 +0800

    modified:   bucket/aria2.json 	deleted:    bucket/bedrock-server-nssm.json 	modified:   bucket/bg3modmanager.json 	modified:   bucket/ccleaner.json 	modified:   bucket/cheat-engine.json 	deleted:    bucket/chinesesubfinder-nssm.json 	deleted:    bucket/clash-verge-rev.json 	modified:   bucket/cuda-cudnn.json 	modified:   bucket/displaycal.json 	modified:   bucket/dlss-swapper.json 	modified:   bucket/eudic.json 	modified:   bucket/exhyperv.json 	deleted:    bucket/fehviewer.json 	deleted:    bucket/imfile.json 	deleted:    bucket/jellyfin-media-player.json 	deleted:    bucket/jellyfin-nssm.json 	modified:   bucket/jhentai.json 	modified:   bucket/modorganizer.json 	new file:   bucket/motrix-next.json 	deleted:    bucket/moviepilot-nssm.json 	modified:   bucket/mpcvideorenderer.json 	deleted:    bucket/mycomputermanager.json 	modified:   bucket/ns-emu-tools.json 	deleted:    bucket/old/.gitkeep 	modified:   bucket/potplayer-mini.json 	deleted:    bucket/qbittorrent-nssm.json 	modified:   bucket/qemu-img.json 	modified:   bucket/setdpi.json 	modified:   bucket/shanaencoder.json 	deleted:    bucket/sidequest.json 	modified:   bucket/skif.json 	modified:   bucket/subrenamer.json 	modified:   bucket/sudppipe.json 	modified:   bucket/ttime.json 	modified:   bucket/uninstall-tool.json 	modified:   bucket/wabbajack.json 	modified:   bucket/winapp2.json 	deleted:    bucket/winauth.json 	modified:   bucket/winscp-localized.json 	modified:   bucket/yuzumoddownloader.json 	deleted:    scripts/DoradoUtils.psm1 	new file:   scripts/utils.psm1

---
+++ scripts/utils.psm1
@@ -0,0 +1,114 @@
+#Requires -Version 5.1
+Set-StrictMode -Version 3.0
+
+function Initialize-ExternalRuntimeData {
+    <#
+    .SYNOPSIS
+        Initialize external runtime data before installation
+    .PARAMETER Source
+        The source path, which is the persist runtime data path
+    .PARAMETER Target
+        The target path, which is the actual path app uses to access the runtime data
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string] $Source,
+        [Parameter(Mandatory = $true, Position = 1)]
+        [string] $Target
+    )
+    if (-not (Test-Path $Source)) {
+        New-Item (Split-Path $Source -Parent) -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+        New-Item $Source -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+        Move-Item -Path $Target -Destination $Source -ErrorAction SilentlyContinue
+    } else {
+        Remove-Item -Path $Target -Recurse -ErrorAction SilentlyContinue
+    }
+}
+
+function Mount-ExternalRuntimeData {
+    <#
+    .SYNOPSIS
+        Mount external runtime data
+
+    .PARAMETER Source
+        The source path, which is the persist_dir
+
+    .PARAMETER Target
+        The target path, which is the actual path app uses to access the runtime data
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string] $Source,
+        [Parameter(Mandatory = $true, Position = 1)]
+        [string] $Target
+    )
+
+    if (Test-Path $Source) {
+        Remove-Item $Target -Force -Recurse -ErrorAction SilentlyContinue
+    } else {
+        New-Item -ItemType Directory $Source -Force | Out-Null
+        if (Test-Path $Target) {
+            Get-ChildItem $Target | Move-Item -Destination $Source -Force
+            Remove-Item $Target
+        }
+    }
+
+    New-Item -ItemType Junction -Path $Target -Target $Source -Force | Out-Null
+}
+
+function Dismount-ExternalRuntimeData {
+    <#
+    .SYNOPSIS
+        Unmount external runtime data
+
+    .PARAMETER Target
+        The target path, which is the actual path app uses to access the runtime data
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string] $Target
+    )
+
+    if (Test-Path $Target) {
+        Remove-Item $Target -Force -Recurse
+    }
+}
+
+function Stop-App {
+    param (
+        [Parameter(
+            Position = 0,
+            ValueFromPipeline,
+            HelpMessage = "Array of paths to search for executables"
+        )]
+        [string[]]
+        $Path
+    )
+
+    if (-not $Path) {
+        $Path = @(
+            $dir,
+            (Split-Path $dir -Parent) + '\current'
+        )
+    }
+
+    $allProcesses = Get-Process
+
+    foreach ($app_dir in $Path) {
+        $allProcesses |
+        Where-Object {
+            $_.Modules.FileName -like "$app_dir\*"
+        } |
+        ForEach-Object {
+            Stop-Process -Id $_.Id -Force -ErrorAction SilentlyContinue
+            Wait-Process -Id $_.Id -ErrorAction SilentlyContinue -Timeout 30
+        }
+    }
+}
+
+Export-ModuleMember `
+    -Function `
+    Initialize-ExternalRuntimeData, Mount-ExternalRuntimeData, Dismount-ExternalRuntimeData, Stop-App
